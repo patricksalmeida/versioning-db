6ba76fba9803d841ce7efbe6dcf2fc0ddf1a618d
Patrick Almeida <patricksalmeida@gmail.com>
2022-07-21 00:59:15 -0300

    feat: add timestamps columns

---
+++ db/migration/V1.4.0__add_timestamp_columns_table_users_and_tasks.sql
@@ -0,0 +1,5 @@
+alter table users add column created_at timestamp after situation;
+alter table users add column updated_at timestamp after created_at;
+
+alter table tasks add column created_at timestamp after situation;
+alter table tasks add column updated_at timestamp after created_at;
